1de8f61f360680114727c61756bde483a2e359f7
Anoop Naravaram <anoop901@gmail.com>
2020-05-02 22:32:59 -0500

    Remove placeholder text.

--- src/components/BlockBrowser.tsx
+++ src/components/BlockBrowser.tsx
@@ -4,9 +4,6 @@ import './BlockBrowser.css'
 export default class BlockBrowser extends React.Component {
   render() {
     return <div className="BlockBrowser">
-      {Array.from({length: 100}).map((_, i) => 
-        <div>Block #{i}</div>
-      )}
     </div>
   }
 }
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -4,9 +4,6 @@ import './CodeEditor.css'
 export default class CodeEditor extends React.Component {
   render() {
     return <div className="CodeEditor">
-      {Array.from({length: 200}).map((_, i) =>
-        <div>Code #{i}</div>
-      )}
     </div>
   }
 }
