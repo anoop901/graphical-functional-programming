1410a0596d7349d6bf62a377b73257089661f088
Anoop Naravaram <anoop901@gmail.com>
2020-05-02 19:07:37 -0500

    Setup react.

---
+++ src/components/IndexPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        Put contents of index page here.
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,7 @@
-const message: string = "hello world";
-console.log(message);
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import IndexPage from './components/IndexPage';
+
+const divElem: HTMLElement = document.createElement('div');
+document.body.appendChild(divElem);
+ReactDOM.render(<IndexPage />, divElem);
