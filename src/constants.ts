c43bebdd9c92f74951f458ab85646e7be31be90b
Anoop Naravaram <anoop901@gmail.com>
2020-05-25 15:29:43 -0500

    Render different types of blocks differently.
    Also created a convenience function buildSvgPath for constructing SVG path strings.

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const minimumTerminalSpacing = 50;
+export const notchHalfWidth = 5;
+export const notchHeight = 10;
+export const blockHeight = 40;
+export const functionBlockRoundPartLength = 10;
