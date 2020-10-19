b9b260fd188951b8f25dd6ad05eebc9ed0c76c82
Anoop Naravaram <anoop901@gmail.com>
2020-10-19 15:55:31 -0500

    Add deploy workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   plugins: [
     new HTMLWebpackPlugin({
-      title: 'Graphical Functional Programming',
-      meta: {viewport: 'width=device-width, initial-scale=1'}
-    })
+      title: "Graphical Functional Programming",
+      meta: { viewport: "width=device-width, initial-scale=1" },
+    }),
   ],
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
+  output: { publicPath: "/graphical-functional-programming" },
 };
