bf45396a44a10b62d179081653edbc73a68cb836
Anoop Naravaram <anoop901@gmail.com>
2021-08-30 09:33:35 -0500

    create store and provide it

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import IndexPage from "./components/IndexPage";
 import "./index.css";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const divElem: HTMLElement = document.createElement("div");
 document.body.appendChild(divElem);
-ReactDOM.render(<IndexPage />, divElem);
+ReactDOM.render(
+  <Provider store={store}>
+    <IndexPage />
+  </Provider>,
+  divElem
+);
