e9b0ff4d2760b3b86dfd5dc606ae91d26a654898
Anoop Naravaram <anoop901@gmail.com>
2023-07-23 14:24:28 -0700

    factor out ResizingSvg component

--- components/ProgramEditor.tsx
+++ components/ProgramEditor.tsx
@@ -2,32 +2,13 @@
 import colors from "tailwindcss/colors";
 import CenteredRect from "./CenteredRect";
 import { useEffect, useRef, useState } from "react";
+import ResizingSvg from "./ResizingSvg";
 
 export default function ProgramEditor() {
-  const svgRef = useRef<SVGSVGElement>(null);
-  const [size, setSize] = useState({ width: 0, height: 0 });
-
-  useEffect(() => {
-    if (svgRef.current) {
-      const observer = new ResizeObserver((e) => {
-        const { inlineSize: width, blockSize: height } = e[0].contentBoxSize[0];
-        setSize({ width, height });
-      });
-      observer.observe(svgRef.current);
-
-      return () => observer.disconnect();
-    }
-  }, []);
   const center = { x: 0, y: 0 };
 
   return (
-    <svg
-      viewBox={`${-size.width / 2} ${-size.height / 2} ${size.width} ${
-        size.height
-      }`}
-      ref={svgRef}
-      className="h-full"
-    >
+    <ResizingSvg>
       <CenteredRect
         x={center.x}
         y={center.y}
@@ -45,8 +26,8 @@ export default function ProgramEditor() {
         textAnchor="middle"
         alignmentBaseline="middle"
       >
-        {size.width}x{size.height}
+        foo
       </text>
-    </svg>
+    </ResizingSvg>
   );
 }
---
+++ components/ResizingSvg.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function ResizingSvg({ children }: { children: any }) {
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  useEffect(() => {
+    if (svgRef.current) {
+      const observer = new ResizeObserver((e) => {
+        const { inlineSize: width, blockSize: height } = e[0].contentBoxSize[0];
+        setSize({ width, height });
+      });
+      observer.observe(svgRef.current);
+
+      return () => observer.disconnect();
+    }
+  }, []);
+  return (
+    <svg
+      viewBox={`${-size.width / 2} ${-size.height / 2} ${size.width} ${
+        size.height
+      }`}
+      ref={svgRef}
+      className="h-full"
+    >
+      {children}
+    </svg>
+  );
+}
