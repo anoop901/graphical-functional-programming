732c87e0fdd76941146315fbeabb95a36a5048bf
Anoop Naravaram <anoop901@gmail.com>
2023-07-24 16:50:09 -0700

    generic block handler

--- model/Block.ts
+++ model/Block.ts
@@ -4,3 +4,24 @@ import ReferenceBlock from "./ReferenceBlock";
 
 type Block = IntegerLiteralBlock | ReferenceBlock | FunctionCallBlock;
 export default Block;
+
+interface BlockHandlers<R> {
+  handleIntegerLiteralBlock(block: IntegerLiteralBlock): R;
+  handleReferenceBlock(block: ReferenceBlock): R;
+  handleFunctionCallBlock(block: FunctionCallBlock): R;
+}
+
+function getGenericBlockHandler<R>(
+  handlers: BlockHandlers<R>
+): (block: Block) => R {
+  return (block: Block) => {
+    switch (block.type) {
+      case "IntegerLiteralBlock":
+        return handlers.handleIntegerLiteralBlock(block);
+      case "ReferenceBlock":
+        return handlers.handleReferenceBlock(block);
+      case "FunctionCallBlock":
+        return handlers.handleFunctionCallBlock(block);
+    }
+  };
+}
