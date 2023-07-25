c20a6ff0a361c70fb45f59bafecaf06afbda738a
Anoop Naravaram <anoop901@gmail.com>
2023-07-24 17:21:02 -0700

    update initial program

--- model/Program.ts
+++ model/Program.ts
@@ -11,22 +11,42 @@ export function makeInitialProgram(): Program {
   const blockId4 = window.crypto.randomUUID();
   const blockId5 = window.crypto.randomUUID();
   const blockId6 = window.crypto.randomUUID();
+  const blockId7 = window.crypto.randomUUID();
+  const blockId8 = window.crypto.randomUUID();
+  const blockId9 = window.crypto.randomUUID();
+  const blockId10 = window.crypto.randomUUID();
   return {
     blocks: {
       [blockId1]: { type: "IntegerLiteralBlock", value: 10 },
       [blockId2]: { type: "IntegerLiteralBlock", value: 20 },
       [blockId3]: { type: "IntegerLiteralBlock", value: 30 },
       [blockId4]: {
-        type: "PlusBlock",
-        arg1BlockId: blockId1,
-        arg2BlockId: blockId2,
+        type: "ReferenceBlock",
+        name: "plus",
       },
       [blockId5]: {
-        type: "TimesBlock",
-        arg1BlockId: blockId3,
-        arg2BlockId: blockId4,
+        type: "ArrayBlock",
+        elementBlockIds: [blockId1, blockId2],
       },
-      [blockId6]: { type: "IntegerLiteralBlock", value: 40 },
+      [blockId6]: {
+        type: "FunctionCallBlock",
+        functionBlockId: blockId4,
+        argumentBlockId: blockId5,
+      },
+      [blockId7]: { type: "IntegerLiteralBlock", value: 40 },
+      [blockId8]: {
+        type: "ReferenceBlock",
+        name: "times",
+      },
+      [blockId9]: {
+        type: "ArrayBlock",
+        elementBlockIds: [blockId6, blockId7],
+      },
+      [blockId10]: {
+        type: "FunctionCallBlock",
+        functionBlockId: blockId8,
+        argumentBlockId: blockId9,
+      },
     },
   };
 }
